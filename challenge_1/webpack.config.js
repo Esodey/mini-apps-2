1a1a0593e66dab3c44fe9a0f039d45fb4d6c86eb
Erik Soderholm <esodey@gmail.com>
2019-06-25 14:04:54 -0700

    Set up the db.json and server-json. Still working on setting up webpack

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/public/index.html');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  devtool: "source-map",
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      { 
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react'],
+        }
+      }
+    ]
+  }
+}
